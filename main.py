5ae7d1ce5a0d6c1e0709283b9e1f13e38c7fd6f3
PauloSantanaPC <118673327+PauloSantanaPC@users.noreply.github.com>
2022-11-23 16:07:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -277,10 +277,11 @@ def horarioJogoGrupo(nomeGrupo,nomeJogo):
         if nomeJogo == 0:
             # Catar x Equador
             #inicioJogo = horarioJogo(2022,11,20,13,0)
-            inicioJogo = horarioJogo(2022,11,20,23,0)
+            inicioJogo = horarioJogo(2022,11,23,23,59)
         elif nomeJogo == 1:
             # Senegal X Holanda
-            inicioJogo = horarioJogo(2022,11,21,13,0)
+            #inicioJogo = horarioJogo(2022,11,21,13,0)
+            inicioJogo = horarioJogo(2022,11,23,23,59)
         elif nomeJogo == 2:
             # Catar X Senegal
             inicioJogo = horarioJogo(2022,11,25,10,0)
@@ -298,10 +299,12 @@ def horarioJogoGrupo(nomeGrupo,nomeJogo):
         # Grupo B
         if nomeJogo == 0:
             # Inglaterra X Irã
-            inicioJogo = horarioJogo(2022,11,21,10,0)
+            #inicioJogo = horarioJogo(2022,11,21,10,0)
+            inicioJogo = horarioJogo(2022,11,23,23,59)
         elif nomeJogo == 1:
             # Estados Unidos X País de Gales
-            inicioJogo = horarioJogo(2022,11,21,16,0)
+            #inicioJogo = horarioJogo(2022,11,21,16,0)
+            inicioJogo = horarioJogo(2022,11,23,23,59)
         elif nomeJogo == 2:
             # Inglaterra X Estados Unidos
             inicioJogo = horarioJogo(2022,11,25,16,0)
