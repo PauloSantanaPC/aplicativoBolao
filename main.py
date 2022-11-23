8a3cc8fc1df133ad88089eee96c716be0a089ef2
PauloSantanaPC <118673327+PauloSantanaPC@users.noreply.github.com>
2022-11-23 11:14:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1163,7 +1163,83 @@ def main():
                                     
                             else:
                                 with tabs[usuario]:
-                                    st.header(f'Resumo das apostas - {np.array(usuariosLista)[usuario][0]}')
+                                    #st.header(f'Resumo das apostas - {np.array(usuariosLista)[usuario][0]}')
+                                    
+                                    dataHoraMinutoAtual = datetime.strptime(datetime.now(pytz.timezone('America/Sao_Paulo')).strftime('%d/%m/%y %H:%M'), '%d/%m/%y %H:%M')
+                                    st.header(f'Resumo das apostas {dataHoraMinutoAtual} - {np.array(usuariosLista)[usuario][0]}')                                    
+                                    st.subheader(f'Apostas jogos primeira fase:')
+                                    for contadorGrupo in range(8):
+                                        st.write(f'Grupo {grupos()[:,4][contadorGrupo]}')
+                                        for contadorJogo in range(6):
+                                            # rodada e jogo
+                                            if contadorJogo == 0 or contadorJogo == 1:
+                                                nomeRodada = 1
+                                            elif contadorJogo == 2 or contadorJogo == 3:
+                                                nomeRodada = 2
+                                            elif contadorJogo == 4 or contadorJogo == 5:
+                                                nomeRodada = 3
+
+
+                                            if nomeRodada == 1:
+                                                # Time i1 = 0
+                                                # Time i2 = 1
+                                                # Time i3 = 2
+                                                # Time i4 = 3
+                                                # rodada 1: Time i1 x Time i2
+                                                # rodada 1: Time i3 x Time i4
+                                                time1 = 0
+                                                time2 = 1
+                                                time3 = 2
+                                                time4 = 3
+                                                if contadorJogo == 0:
+                                                    timeMandante  = time1
+                                                    timeVisitante = time2
+                                                elif contadorJogo == 1:
+                                                    timeMandante  = time3
+                                                    timeVisitante = time4
+
+                                            elif nomeRodada == 2:
+                                                # Time i1 = 0
+                                                # Time i2 = 1
+                                                # Time i3 = 2
+                                                # Time i4 = 3
+                                                # rodada 2: Time i1 x Time i3
+                                                # rodada 2: Time i2 x Time i4
+                                                time1 = 0
+                                                time2 = 2
+                                                time3 = 1
+                                                time4 = 3
+                                                if contadorJogo == 2:
+                                                    timeMandante  = time1
+                                                    timeVisitante = time2
+                                                elif contadorJogo == 3:
+                                                    timeMandante  = time3
+                                                    timeVisitante = time4
+
+                                            elif nomeRodada == 3:
+                                                # Time i1 = 0
+                                                # Time i2 = 1
+                                                # Time i3 = 2
+                                                # Time i4 = 3
+                                                # rodada 3: Time i4 x Time i1
+                                                # rodada 3: Time i2 x Time i3
+                                                time1 = 3
+                                                time2 = 0
+                                                time3 = 1
+                                                time4 = 2
+                                                if contadorJogo == 4:
+                                                    timeMandante  = time1
+                                                    timeVisitante = time2
+                                                elif contadorJogo == 5:
+                                                    timeMandante  = time3
+                                                    timeVisitante = time4
+
+                                            if not horarioJogoGrupo(contadorGrupo,contadorJogo):
+                                                if np.array(usuariosLista)[usuario][28+2*6*contadorGrupo+2*contadorJogo] != '':
+                                                    st.write(f'Jogo {contadorJogo+1}: {grupos()[contadorGrupo][timeMandante]} {np.array(usuariosLista)[usuario][28+2*6*contadorGrupo+2*contadorJogo]} x {np.array(usuariosLista)[usuario][29+2*6*contadorGrupo+2*contadorJogo]} {grupos()[contadorGrupo][timeVisitante]}')
+                                                else:
+                                                    st.write(f'Jogo {contadorJogo+1}: Aposta NÃO realizada.')
+
                                     if usuariosLista[usuario][8] != '':
                                         opcoesBolao = ['Campeão do mundo','Vice de nada','cara que não sabe de futebol, mas não vai ser o pior do bolão','Pangaré do futebol']
                                         st.subheader(f'Acha que vai ser o {opcoesBolao[int(usuariosLista[usuario][8])]} !')
