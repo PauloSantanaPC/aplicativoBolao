9177710fc36a531c193073c0b967abc931572515
PauloSantanaPC <118673327+PauloSantanaPC@users.noreply.github.com>
2022-12-04 12:02:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1982,8 +1982,8 @@ def main():
                         horarioOitavas2 = horarioJogo(2022,12,3,16,0)
                         horarioOitavas3 = horarioJogo(2022,12,5,12,0)
                         horarioOitavas4 = horarioJogo(2022,12,5,16,0)
-                        horarioOitavas5 = horarioJogo(2022,12,4,12,0)
-                        horarioOitavas6 = horarioJogo(2022,12,4,16,0)
+                        horarioOitavas5 = horarioJogo(2022,12,4,16,0)
+                        horarioOitavas6 = horarioJogo(2022,12,4,12,0)
                         horarioOitavas7 = horarioJogo(2022,12,6,12,0)
                         horarioOitavas8 = horarioJogo(2022,12,6,16,0)
                         horarioOitavas  = [horarioOitavas1,
