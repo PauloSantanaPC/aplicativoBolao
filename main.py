a6b684c05238014172ae6332072fa44232e1ee25
PauloSantanaPC <118673327+PauloSantanaPC@users.noreply.github.com>
2022-12-17 15:02:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2266,7 +2266,7 @@ def apostasTerceiro(usuario,nomeUsuario,usuarioMestre):
     #-----------------------------
     opcoesTerceiro = ['Croácia','Marrocos']
     #-----------------------------
-    horarioTerceiro = [horarioJogo(2022,12,17,15,1)]
+    horarioTerceiro = [horarioJogo(2022,12,17,15,10)]
     #-----------------------------
     dataTerceiro = datetime(2022,12,17,12,0)
     #-----------------------------
