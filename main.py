634f9720a2b696464447a41f7695763001be8cb0
PauloSantanaPC <118673327+PauloSantanaPC@users.noreply.github.com>
2022-12-17 16:20:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1687,7 +1687,7 @@ def classificacaoBolaoGrupos():
         else:
             pontuacaoFinal += 0
 
-        if not horario:
+        if horario:
             pontuacaoFinalUsuarios.append(pontuacaoFinal)
         else:
             pontuacaoFinalUsuarios.append('-')        
@@ -1705,7 +1705,7 @@ def classificacaoBolaoGrupos():
         else:
             pontuacaoTerceira += 0
 
-        if not horario:
+        if horario:
             pontuacaoTerceiraUsuarios.append(pontuacaoTerceira)
         else:
             pontuacaoTerceiraUsuarios.append('-')
